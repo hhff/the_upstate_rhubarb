db004e2de5fdb9b9fbc5bc81fc4083d6666c92fb
Hugh Francis <me@hughfrancis.me>
2014-07-07 19:11:44 -0400

    First Push

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	var container = document.getElementById("container");
+	var logo = document.getElementById("logo");
+	var text = document.getElementById("text");
+	classie.remove(container, 'hidden');
+	classie.remove(logo, 'hidden');
+	classie.remove(text, 'hidden');
+};
